468e2e9952afa57635cf86797c09d0948ff87e1f
irshad <irshad.ali@fortunapix.com>
2023-04-05 16:33:23 +0530

    Orientation feature added

--- src/components/DeviceOrientation.svelte
+++ src/components/DeviceOrientation.svelte
@@ -10,10 +10,16 @@
   function unlockOrientation() {
     window.screen.orientation.unlock();
   }
+
+  let orientation = ''
+
+  window.addEventListener("orientationchange", function () {
+    orientation = screen.orientation.type
+  });
 </script>
 
 <div class="poc-section">
-  <p>4) Device Rotation</p>
+  <p>4) Device Rotation ({orientation})</p>
   <div class="d-flex gap-2">
     <button class="btn btn-dark mt-2" on:click={landscape}> Landscape </button>
     <button class="btn btn-dark mt-2" on:click={portrait}> Portrait </button>
